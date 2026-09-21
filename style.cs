4da050cba3aea8e9ab4bf75225f1f0362355b9cb
anabigettedias-ops <ana.bigette.dias@escola.pr.gov.br>
2026-09-21 11:05:08 -0300

    Update style.cs

--- style.cs
+++ style.cs
@@ -1,1 +1,12 @@
+const botoes = document.querSelector("button");
+botoes.
+botoes.forEach(Function(botao){
+botoes.addEventListener("click",botaoClicado);
+function  botaoClicado() {
+  console.long("fui clicado");
+  let texto = botao.querySelector("span");
+  if (curtiu === false){
+  }
+   texto.textcontent++;}
 
+});
